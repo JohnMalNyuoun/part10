c6b6dbfa5a8c58e0571fac1976c17754fd7d71c1
johnmalnyuon <wmal44884@gmail.com>
2026-09-07 11:55:45 +0300

    10.4The app bar

---
+++ rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,28 @@
+import { View, StyleSheet, Text, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#24292e',
+    paddingBottom: 15,
+    paddingLeft: 15,
+  },
+  text: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <Pressable>
+        <Text style={styles.text}>Repositories</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default AppBar;
--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,9 +1,19 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import RepositoryList from './RepositoryList';
+import AppBar from './AppBar';
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    flexShrink: 1,
+    backgroundColor: '#e1e8ed',
+  },
+});
 
 const Main = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <AppBar />
       <RepositoryList />
     </View>
   );
