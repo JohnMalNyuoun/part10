8507ba097a6e388c908ffe79ae4037e19f9b831a
johnmalnyuon <wmal44884@gmail.com>
2026-09-08 13:59:51 +0300

    Convert rate-repository-api to standard folder

---
+++ rate-repository-api/knexfile.js
@@ -0,0 +1,7 @@
+export default {
+  client: 'better-sqlite3',
+  connection: {
+    filename: './database.sqlite',
+  },
+  useNullAsDefault: true,
+};
---
+++ rate-repository-api/src/utils/knex.js
@@ -0,0 +1,7 @@
+import knex from 'knex';
+
+import knexfile from '../../knexfile.js';
+
+const db = knex(knexfile);
+
+export default db;
